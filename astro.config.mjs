a99d099743ca1b542a2e2c8540abe3959be81cd4
mrzcookie <91168951+mrzcookie@users.noreply.github.com>
2025-07-04 02:01:02 -0500

    feat: add TailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
 	site: "https://www.mrzcookie.dev",
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	build: {
 		assets: "assets",
 	},
